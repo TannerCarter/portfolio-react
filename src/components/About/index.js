bb394960345abf35ae01c8345b45ea2ff62230d3
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-09-04 22:30:14 -0700

    Added About component

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">Hello On The Screen!!!</h1>
+      <p className="my-2" style={{ width: "100%" }}>
+        Hello
+      </p>
+    </section>
+  );
+}
+
+export default About;

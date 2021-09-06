ca7769d231c69d009f4c33ac9db6cccd8187cf69
TannerCarter <80929740+TannerCarter@users.noreply.github.com>
2021-09-05 22:19:41 -0700

    Nav Component setup, revised css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,69 @@
 import React from "react";
+import avatarImage from "../../assets/header/Tanner.jpg";
 
 function About() {
   return (
-    <section>
-      <h1 id="about">Hello On The Screen!!!</h1>
+    <section className="flex-row">
+      <h1 id="about" className="flex-row">
+        About Me{" "}
+      </h1>
+      <img src={avatarImage} alt="Tanner Carter" className="avatar" />
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare
+        dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
+        Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin
+        nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo
+        vel turpis sollicitudin, et tristique velit convallis. In hac habitasse
+        platea dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+        Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac
+        habitasse platea dictumst.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare
+        dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
+        Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin
+        nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo
+        vel turpis sollicitudin, et tristique velit convallis. In hac habitasse
+        platea dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+        Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac
+        habitasse platea dictumst.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare
+        dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
+        Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin
+        nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo
+        vel turpis sollicitudin, et tristique velit convallis. In hac habitasse
+        platea dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+        Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac
+        habitasse platea dictumst.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare
+        dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
+        Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin
+        nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo
+        vel turpis sollicitudin, et tristique velit convallis. In hac habitasse
+        platea dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+        Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac
+        habitasse platea dictumst.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum
+        turpis sed ex condimentum molestie. Mauris condimentum lectus ut ornare
+        dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam maximus.
+        Proin aliquam justo nec diam vulputate vestibulum. Aenean sollicitudin
+        nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique justo
+        vel turpis sollicitudin, et tristique velit convallis. In hac habitasse
+        platea dictumst. Phasellus mattis nunc sed orci consequat laoreet.
+        Praesent id nisl nibh. Curabitur imperdiet ultricies mollis. In hac
+        habitasse platea dictumst.
+      </p>
     </section>
   );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,45 +1,34 @@
 import React from "react";
 
 function Nav() {
-  const categories = [
-    {
-      name: "Contact",
-      description: "Form to contact Tanner Carter, links to reach out",
-    },
-    { name: "Resume", description: "Tanner Carter's Resume" },
-  ];
-
-  function categorySelected(name) {
-    console.log(`${name} clicked`);
-  }
-
   return (
-    <header>
-      <h2>
-        <a href="/">
-          <span role="img" aria-label=""></span>
-          Tanner Carter
-        </a>
+    <nav className="flex-row">
+      <h2 className="navBar">
+        <a href="/">Tanner Carter</a>
       </h2>
-
-      <nav>
-        <ul className="flex-row">
-          <li className="mx-2">
-            <a href="#about">About Me</a>
-          </li>
-          <li>
-            <span>Portfolio</span>
-          </li>
-          {categories.map((category) => (
-            <li className="mx-1" key={category.name}>
-              <span onClick={() => categorySelected(category.name)}>
-                {category.name}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+      <ul className="mx-2 flex-row">
+        <li>
+          <a href="#about" className="mx-2">
+            <button>About Me</button>
+          </a>
+        </li>
+        <li>
+          <a href="#portfolio" className="mx-2">
+            <button>Portfolio</button>
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className="mx-2">
+            <button>Contact</button>
+          </a>
+        </li>
+        <li>
+          <a href="#resume" className="mx-2">
+            <button>Resume</button>
+          </a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
